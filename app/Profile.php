2585712433197e555467ec470c86f0bb0d844cbf
Eric <liwei03116@hotmail.com>
2020-03-03 14:05:29 +1100

    First commit

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'uploads/evUu2Z0e5eibw3ALDwe0aifV8NySHpAP5d5VFn1v.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
